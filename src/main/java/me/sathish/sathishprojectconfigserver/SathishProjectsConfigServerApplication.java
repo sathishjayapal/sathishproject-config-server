7f436a1c37679dcd2e50fd292babe9378942c17a
Sathish K Jayapal <203055+sathishjayapal@users.noreply.github.com>
2022-05-26 22:50:34 -0500

    Moved to separate repo

---
+++ src/main/java/me/sathish/sathishprojectconfigserver/SathishProjectsConfigServerApplication.java
@@ -0,0 +1,14 @@
+package me.sathish.sathishprojectconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SathishProjectsConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SathishProjectsConfigServerApplication.class, args);
+    }
+
+}
