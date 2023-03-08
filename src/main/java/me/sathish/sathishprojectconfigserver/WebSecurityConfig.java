b57e8c28c75990e00a1863e57e843d117139c2ec
Sathish Jayapal <203055+sathishjayapal@users.noreply.github.com>
2023-03-07 22:25:28 -0600

    Config server details

---
+++ src/main/java/me/sathish/sathishprojectconfigserver/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package me.sathish.sathishprojectconfigserver;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity(debug = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().
+                antMatchers("/encrypt").permitAll().
+                anyRequest().authenticated().and().csrf().disable().httpBasic();
+    }
+
+}
