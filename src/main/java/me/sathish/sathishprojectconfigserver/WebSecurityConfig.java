ed8923035d578254b08eb34c3103f9e08b618342
Sathish Jayapal <sjayapal@skminfotech.com>
2023-03-13 22:19:05 -0500

    externalize system props

--- src/main/java/me/sathish/sathishprojectconfigserver/WebSecurityConfig.java
+++ src/main/java/me/sathish/sathishprojectconfigserver/WebSecurityConfig.java
@@ -1,16 +1,38 @@
 package me.sathish.sathishprojectconfigserver;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@EnableWebSecurity(debug = false)
+@EnableWebSecurity(debug = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    final
+    BasicAuthBean environment;
 
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().
-                antMatchers("/encrypt").permitAll().
-                anyRequest().authenticated().and().csrf().disable().httpBasic();
+    public WebSecurityConfig(BasicAuthBean environment) {
+        this.environment = environment;
     }
 
+    //
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser(environment.getUsername())
+                .password(environment.getPassword())
+                .roles("ADMIN")
+        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
 }
+
+
