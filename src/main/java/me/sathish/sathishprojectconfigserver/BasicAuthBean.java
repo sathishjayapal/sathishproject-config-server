d189e005635aeecafa325cebc01b11e4548149eb
Sathish Jayapal <sjayapal@skminfotech.com>
2024-06-02 16:24:11 -0500

    config directory established

--- src/main/java/me/sathish/sathishprojectconfigserver/BasicAuthBean.java
+++ src/main/java/me/sathish/sathishprojectconfigserver/BasicAuthBean.java
@@ -1,14 +1,13 @@
 package me.sathish.sathishprojectconfigserver;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BasicAuthBean {
-
     @Value("${pass}")
     private String password;
+
     @Value("${username}")
     private String username;
 
@@ -21,14 +20,10 @@ public class BasicAuthBean {
     }
 
     public String getPassword() {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String encodedPassword = encoder.encode(this.password);
-        return encodedPassword;
+        return this.password;
     }
 
     public void setPassword(String password) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String encodedPassword = encoder.encode(password);
-        this.password = encodedPassword;
+        this.password = password;
     }
 }
